e37aa45614dd4a8046b658957aa3ca12718df94b
shahdatef <shahdatef346@gmail.com>
2024-12-01 15:35:02 +0200

    first commit

---
+++ OnlineCenter/Program.cs
@@ -0,0 +1,64 @@
+using DataAccess;
+using DataAccess.Reposetory;
+using DataAccess.Reposetory.IReposetory;
+using DataAccess.Repository;
+using DataAccsess.Reposetory.IReposetory;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace OnlineCenter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
+            builder.Services.AddDbContext<ApplicationDbContext>(
+            option => option.UseSqlServer(builder.Configuration.GetConnectionString("Defaultconnection")));
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDbContext>();
+            //builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+            builder.Services.AddScoped<ITeacherRepository,TeacherRepository>();
+            builder.Services.AddScoped<ISubjectRepsitory, SubjectRepository>();
+            builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+            builder.Services.AddScoped<ILectureRepository, LectureRepository>();
+            builder.Services.AddScoped<IGradeRepository, GradeRepository>();
+            builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+            builder.Services.AddScoped<ICartRepository, CartRepository>();
+            builder.Services.AddScoped<IBookRepository, BookRepository>();
+            builder.Services.AddScoped<IAssistantRepository, AssistantRepository>();
+            builder.Services.AddScoped<IAnswerRepository, AnswerRepository>();
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.MapRazorPages();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{area=Student}/{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
